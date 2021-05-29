7703dd318744222ce3a94de4f86df48c3baea4ee
luoxifeng <942394682@qq.com>
2021-05-29 23:22:35 +0800

    fix

--- example/source.vue
+++ example/source.vue
@@ -1,11 +1,20 @@
-<template>
+<template >
   <div>
-    <test-com jiai-register="./test.vue">M</test-com>
-    <test-com1 jiai-register="./test1.vue">M</test-com1>
+    <!-- <test-com jiai-register="./test.vue">M</test-com> -->
+    <test  />
+    <test-com1 jiai-register="./test1.vue">M</test-com1> 
+    <test-com1 jiai-register="./test1.vue">M</test-com1> 
+
+    <test-com1 jiai-register="./test1.vue">M</test-com1> 
+    <test-com1 jiai-register="./test1.vue">M</test-com1> 
+    <test jiai-register="./test.vue">M</test> 
+    <test-com1 jiai-register="./test1.vue">M</test-com1> 
+
   </div>
 </template>
 
 <script>
+
 export default {
   data() {
     return {

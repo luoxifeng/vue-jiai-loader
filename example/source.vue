00c1a7bda5ef110f9a46ceabd9386118be450655
贾健平 <jiajianping@bytedance.com>
2021-05-31 15:53:16 +0800

    wsxpp commit test

--- example/source.vue
+++ example/source.vue
@@ -1,24 +1,22 @@
 <template >
   <div>
     <!-- <test-com jiai-register="./test.vue">M</test-com> -->
-    <test  />
-    <test-com1 jiai-register="./test1.vue">M</test-com1> 
-    <test-com1 jiai-register="./test1.vue">M</test-com1> 
+    <test />
+    <test-com1 jiai-register="./test1.vue">M</test-com1>
+    <test-com1>M</test-com1>
 
-    <test-com1 jiai-register="./test1.vue">M</test-com1> 
-    <test-com1 jiai-register="./test1.vue">M</test-com1> 
-    <test jiai-register="./test.vue">M</test> 
-    <test-com1 jiai-register="./test1.vue">M</test-com1> 
-
+    <test-com1>M</test-com1>
+    <test-com1>M</test-com1>
+    <test jiai-register="./test.vue">M</test>
+    <test-com1>M</test-com1>
   </div>
 </template>
 
 <script>
-
 export default {
   data() {
     return {
-      msg: "fesdff111dddsddssddffgc"
+      msg: "fesdff111dddsddssddffgc",
     };
   },
 };

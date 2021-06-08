71c9ddcd5172d21546761691cf3dcf18badda049
luoxifeng <942394682@qq.com>
2021-06-08 22:15:19 +0800

    update new grammar

--- example/source.vue
+++ example/source.vue
@@ -1,18 +1,25 @@
 <template>
   <div>
-    <test-com jiai-register="./test.vue">M</test-com>
+    <test-com jiai-on="./test.vue">M</test-com>
     <test />
-    <test-com1 jiai-register="./test1.vue">M</test-com1>
+    <test-com1 jiai="./test1.vue">M</test-com1>
     <test-com1>M</test-com1>
 
     <test-com1>M</test-com1>
     <test-com1>M</test-com1>
-    <test jiai-register="./test.vue">M</test>
-    <test-com1>M</test-com1>
+    <test jiai="./test.vue" jiai-aa>M</test>
+    <test-com2 jiai-aa>M</test-com2>
     {{msg}}
   </div>
 </template>
+<!--
+jiai-on jiai-in 不能同时出现
 
+jiai-on jiai-in 必须有值
+
+jiai-xxx 分组
+
+-->
 <script  >
 export default {
   data() {
